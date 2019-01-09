d2c35568ae2d498f7fab5e8fa2c8cc0167425833
Yorling <shallowcloud@yeah.net>
2019-01-09 22:38:09 +0800

    project structure init

---
+++ spider.py
@@ -0,0 +1,12 @@
+
+
+class WeiboSpider(object):
+    """
+    used to get some information from weibo with keywords
+    1. use cookies to login
+    2. use weibo's search engine to find data with keywords
+    3. get the users' information in the search results
+    """
+
+    def __init__(self):
+        pass
